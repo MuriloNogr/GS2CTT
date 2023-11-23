3661845e3a9ef0528f6f88983b997a1071ac360c
MuriloNogr <rm89162@fiap.com.br>
2023-11-22 21:33:40 -0300

    GS

--- App.py
+++ App.py
@@ -1,118 +1,192 @@
+from datetime import datetime
 import tkinter as tk
 import cx_Oracle
-from tkinter import Menu
-from tkinter import PhotoImage
-from tkinter import Entry
-from tkinter import Button
-from tkinter import Label
+import json
+from tkinter import messagebox
 
 class Interface:
     def __init__(self, root):
-        self.connection = cx_Oracle.connect('rm89162/280400@oracle.fiap.com.br:1521/orcl')
+        self.connection = cx_Oracle.connect('rm550711/221004@oracle.fiap.com.br:1521/orcl')
         self.cursor = self.connection.cursor()
 
-        root.title("Gerenciador de Dados")
-        root.geometry("800x800")
+        root.title("Gerenciador de Medicamentos")
+        root.geometry("800x1440")
 
         self.registros_text = tk.Text(root, width=80, height=20)
         self.registros_text.pack()
 
-        listar_button = tk.Button(root, text="Listar Registros", command=self.listar_registros)
+        listar_button = tk.Button(root, text="Listar Medicamentos", command=self.listar_medicamentos)
         listar_button.pack()
 
         espaco_vertical = tk.Label(root, text="", height=2)
         espaco_vertical.pack()
 
-        adicionar_label = Label(root, text="Adicionar Registro:")
+        adicionar_label = tk.Label(root, text="Adicionar Medicamento:")
         adicionar_label.pack()
 
-        node_id_label = Label(root, text="Node_ID:")
-        node_id_label.pack()
-        self.node_id_entry = Entry(root)
-        self.node_id_entry.pack()
+        nm_medicamento_label = tk.Label(root, text="Nome do Medicamento:")
+        nm_medicamento_label.pack()
+        self.nm_medicamento_entry = tk.Entry(root)
+        self.nm_medicamento_entry.pack()
 
-        centroide_label = Label(root, text="Centroide:")
-        centroide_label.pack()
-        self.centroide_entry = Entry(root)
-        self.centroide_entry.pack()
+        ds_medicamento_label = tk.Label(root, text="Descrição Detalhada:")
+        ds_medicamento_label.pack()
+        self.ds_medicamento_entry = tk.Entry(root)
+        self.ds_medicamento_entry.pack()
 
-        uf_label = Label(root, text="UF:")
-        uf_label.pack()
-        self.uf_entry = Entry(root)
-        self.uf_entry.pack()
+        codigo_barras_label = tk.Label(root, text="Código de Barras:")
+        codigo_barras_label.pack()
+        self.codigo_barras_entry = tk.Entry(root)
+        self.codigo_barras_entry.pack()
 
-        adicionar_button = Button(root, text="Adicionar", command=self.adicionar_registro)
+        nm_usuario_label = tk.Label(root, text="Nome do Usuário:")
+        nm_usuario_label.pack()
+        self.nm_usuario_entry = tk.Entry(root)
+        self.nm_usuario_entry.pack()
+
+        adicionar_button = tk.Button(root, text="Adicionar", command=self.adicionar_medicamento)
         adicionar_button.pack()
 
-        atualizar_label = Label(root, text="Atualizar Registro:")
+        atualizar_label = tk.Label(root, text="Atualizar Medicamento:")
         atualizar_label.pack()
 
-        indice_label = Label(root, text="Índice:")
-        indice_label.pack()
-        self.indice_entry = Entry(root)
-        self.indice_entry.pack()
+        id_label = tk.Label(root, text="ID do Medicamento:")
+        id_label.pack()
+        self.id_entry = tk.Entry(root)
+        self.id_entry.pack()
 
-        node_id_label = Label(root, text="Node_ID:")
-        node_id_label.pack()
-        self.node_id_entry_atualizar = Entry(root)
-        self.node_id_entry_atualizar.pack()
+        atualizar_nm_medicamento_label = tk.Label(root, text="Novo Nome do Medicamento:")
+        atualizar_nm_medicamento_label.pack()
+        self.atualizar_nm_medicamento_entry = tk.Entry(root)
+        self.atualizar_nm_medicamento_entry.pack()
 
-        centroide_label = Label(root, text="Centroide:")
-        centroide_label.pack()
-        self.centroide_entry_atualizar = Entry(root)
-        self.centroide_entry_atualizar.pack()
+        atualizar_button = tk.Button(root, text="Atualizar", command=self.atualizar_medicamento)
+        atualizar_button.pack(pady=10)
 
-        uf_label = Label(root, text="UF:")
-        uf_label.pack()
-        self.uf_entry_atualizar = Entry(root)
-        self.uf_entry_atualizar.pack()
+        excluir_label = tk.Label(root, text="Excluir Medicamento:")
+        excluir_label.pack()
 
-        atualizar_button = Button(root, text="Atualizar", command=self.atualizar_registro)
-        atualizar_button.pack()
+        id_excluir_label = tk.Label(root, text="ID do Medicamento a Excluir:")
+        id_excluir_label.pack()
+        self.id_excluir_entry = tk.Entry(root)
+        self.id_excluir_entry.pack()
 
-        menu = Menu(root)
+        excluir_button = tk.Button(root, text="Excluir", command=self.excluir_medicamento)
+        excluir_button.pack(pady=5)
+
+        consulta1_button = tk.Button(root, text="Exportar tudo", command=lambda: self.consulta_e_exporta(1))
+        consulta1_button.pack(pady=10)
+
+        consulta2_button = tk.Button(root, text="Exportar nomes", command=lambda: self.consulta_e_exporta(2))
+        consulta2_button.pack(pady=10)
+
+        consulta3_button = tk.Button(root, text="Exportar descrições", command=lambda: self.consulta_e_exporta(3))
+        consulta3_button.pack(pady=10)
+
+        # Menu
+        menu = tk.Menu(root)
         root.config(menu=menu)
 
-        imagem_menu = PhotoImage(file="./images/ResqLogo.png")
-
-        submenu_arquivo = Menu(menu)
+        submenu_arquivo = tk.Menu(menu)
         menu.add_cascade(label="Arquivo", menu=submenu_arquivo)
-        submenu_arquivo.add_command(label="Abrir Imagem", image=imagem_menu, compound=tk.LEFT)
+        submenu_arquivo.add_command(label="Abrir Imagem")
 
-    def listar_registros(self):
+    def listar_medicamentos(self):
         self.registros_text.delete(1.0, tk.END)
-        self.cursor.execute("SELECT * FROM Matrizes")
+        self.cursor.execute("SELECT * FROM T_PLPL_MEDICAMENTO")
         dados = self.cursor.fetchall()
 
-        for i, registro in enumerate(dados):
-            self.registros_text.insert(tk.END, f"Índice: {i}, Node_ID: {registro[0]}, Centroide: {registro[1]}, UF: {registro[2]}\n")
+        for registro in dados:
+            texto = f"ID: {registro[0]}, Nome: {registro[1]}, Descrição: {registro[2]}, "
+            texto += f"Código de Barras: {registro[3]}, Data Cadastro: {registro[4]}, Usuário: {registro[5]}\n"
+            self.registros_text.insert(tk.END, texto)
 
-    def adicionar_registro(self):
-        node_id = self.node_id_entry.get()
-        centroide = self.centroide_entry.get()
-        uf = self.uf_entry.get()
+    def validar_entrada(self, entrada):
+        return entrada.strip() != ""
 
-        try:
-            self.cursor.execute("INSERT INTO Matrizes (Node_ID, Centroide, UF) VALUES (:node_id, :centroide, :uf)",
-                               node_id=node_id, centroide=centroide, uf=uf)
-            self.connection.commit()
-            self.listar_registros()
-        except Exception as e:
-            print("Erro ao adicionar registro:", e)
+    def adicionar_medicamento(self):
+        nm_medicamento = self.nm_medicamento_entry.get()
+        ds_medicamento = self.ds_medicamento_entry.get()
+        codigo_barras = self.codigo_barras_entry.get()
+        nm_usuario = self.nm_usuario_entry.get()
 
-    def atualizar_registro(self):
-        indice = int(self.indice_entry.get())
-        node_id = self.node_id_entry_atualizar.get()
-        centroide = self.centroide_entry_atualizar.get()
-        uf = self.uf_entry_atualizar.get()
+        if not self.validar_entrada(nm_medicamento) or not self.validar_entrada(ds_medicamento) \
+           or not self.validar_entrada(codigo_barras) or not self.validar_entrada(nm_usuario):
+            messagebox.showerror("Erro", "Dados de entrada inválidos")
+            return
 
         try:
-            self.cursor.execute("UPDATE Matrizes SET Node_ID = :node_id, Centroide = :centroide, UF = :uf WHERE Node_ID = :node_id",
-                               node_id=node_id, centroide=centroide, uf=uf)
+            self.cursor.execute("INSERT INTO T_PLPL_MEDICAMENTO (NM_MEDICAMENTO, DS_DETALHADA_MEDICAMENTO, NR_CODIGO_BARRAS, NM_USUARIO) "
+                                "VALUES (:nm_medicamento, :ds_medicamento, :codigo_barras, :nm_usuario)",
+                                nm_medicamento=nm_medicamento, ds_medicamento=ds_medicamento, 
+                                codigo_barras=codigo_barras, nm_usuario=nm_usuario)
             self.connection.commit()
-            self.listar_registros()
+            self.listar_medicamentos()
         except Exception as e:
-            print("Erro ao atualizar registro:", e)
+            messagebox.showerror("Erro ao adicionar medicamento", str(e))
+
+
+    def excluir_medicamento(self):
+        id_medicamento = self.id_excluir_entry.get()
+
+        if not self.validar_entrada(id_medicamento):
+            messagebox.showerror("Erro", "Por favor, insira um ID válido")
+            return
+
+        try:
+            self.cursor.execute("DELETE FROM T_PLPL_MEDICAMENTO WHERE ID_MEDICAMENTO = :ID_MEDICAMENTO",
+                                id_medicamento=id_medicamento)
+            self.connection.commit()
+            messagebox.showinfo("Sucesso", "Medicamento excluído com sucesso")
+            self.listar_medicamentos()
+        except Exception as e:
+            messagebox.showerror("Erro ao excluir medicamento", str(e))
+
+    def atualizar_medicamento(self):
+        id_medicamento = self.id_entry.get()
+        novo_nm_medicamento = self.atualizar_nm_medicamento_entry.get()
+
+        if not self.validar_entrada(id_medicamento) or not self.validar_entrada(novo_nm_medicamento):
+            messagebox.showerror("Erro", "Dados de entrada inválidos")
+            return
+
+        try:
+            self.cursor.execute("UPDATE T_PLPL_MEDICAMENTO SET NM_MEDICAMENTO = :novo_nm_medicamento WHERE ID_MEDICAMENTO = :id_medicamento",
+                                novo_nm_medicamento=novo_nm_medicamento, id_medicamento=id_medicamento)
+            self.connection.commit()
+            self.listar_medicamentos()
+        except Exception as e:
+            messagebox.showerror("Erro ao atualizar medicamento", str(e))
+
+    def consulta_e_exporta(self, numero_consulta):
+        consulta = {
+            1: "SELECT * FROM T_PLPL_MEDICAMENTO",  # Adicione a condição desejada
+            2: "SELECT NM_MEDICAMENTO FROM T_PLPL_MEDICAMENTO",
+            3: "SELECT DS_DETALHADA_MEDICAMENTO FROM T_PLPL_MEDICAMENTO",
+        }[numero_consulta]
+
+        self.cursor.execute(consulta)
+        dados = self.cursor.fetchall()
+        self.exportar_para_json(dados, f'consulta{numero_consulta}.json')
+
+    def exportar_para_json(self, dados, nome_arquivo):
+        try:
+            dados_formatados = []
+            for row in dados:
+                # Convertendo cada valor de data em uma string
+                row_formatado = list(row)
+                for i, valor in enumerate(row_formatado):
+                    if isinstance(valor, datetime):
+                        row_formatado[i] = valor.strftime("%Y-%m-%d %H:%M:%S")
+                dados_formatados.append(dict(zip(["ID_MEDICAMENTO", "NM_MEDICAMENTO", "DS_DETALHADA_MEDICAMENTO", 
+                                                  "NR_CODIGO_BARRAS", "DT_CADASTRO", "NM_USUARIO"], row_formatado)))
+
+            with open(nome_arquivo, 'w') as arquivo:
+                json.dump(dados_formatados, arquivo, indent=4)
+
+            messagebox.showinfo("Sucesso", f"Dados exportados para {nome_arquivo}")
+        except Exception as e:
+            messagebox.showerror("Erro", f"Não foi possível exportar os dados: {e}")
 
 def main():
     root = tk.Tk()
